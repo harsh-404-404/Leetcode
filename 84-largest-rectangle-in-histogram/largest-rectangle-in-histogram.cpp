ed98e304223ecfdc6cfbb37b15ab41d60dccd5be
harsh-404-404 <harshbhalala404@gmail.com>
2026-09-10 18:37:04 +0530

    Time: 6 ms (98.68%) | Memory: 79.4 MB (93.34%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& height) {
-        std::stack<int> stack_left {};
-        std::stack<int> stack_right {};
+        std::stack<int> stack {};
+        int ans = 0;
 
-        std::vector<int> area (height.size(),0);
+        for(int i = 0; i <= height.size(); ++i){
+            
+            int curr = (i == height.size()) ? 0 : height[i];
 
-        for(int i = 0; i < height.size(); ++i){
-            while(!stack_left.empty() && height[stack_left.top()] >= height[i]) stack_left.pop();
-            if(stack_left.empty()) area[i] += i * height[i];
-            else area[i] += (i - stack_left.top() - 1)* height[i];
-            stack_left.push(i);
+            while(!stack.empty() && height[stack.top()] >= curr){
+                int h = height[stack.top()];                
+                stack.pop();
 
-            int j = height.size() - 1 - i;
-            while(!stack_right.empty() && height[stack_right.top()] >= height[j]) stack_right.pop();
-            if(stack_right.empty()) area[j] += (i + 1) * height[j];
-            else area[j] += (stack_right.top() - j)* height[j];
-            stack_right.push(j);
+                int left = (stack.empty()) ? -1 : stack.top(); 
+
+                int width = i - 1 - left;
+                
+                ans = std::max(width*h, ans);
+            }
+
+            stack.push(i);
         }
-
-        return *std::max_element(area.begin(),area.end());
-
-
+        return ans;
     }
 };
