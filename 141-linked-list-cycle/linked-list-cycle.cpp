afb989de5f00473ada5495354d9505bda5e454ee
harsh-404-404 <harshbhalala404@gmail.com>
2026-09-16 13:23:13 +0530

    Time: 9 ms (53.27%) | Memory: 11.9 MB (21.94%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -9,16 +9,16 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if(head == nullptr || head->next == nullptr) return false;
-        ListNode* pt1 = head;
-        ListNode* pt2 = (head->next)->next;
+        ListNode* tortoise = head;
+        ListNode* rabit = head;
 
-        while(pt1 != nullptr && pt2 != nullptr && pt2->next != nullptr && pt1 != pt2){
-            pt1 = pt1->next;
-            pt2 = pt2->next->next;
-        }
-        if(pt1 != nullptr && pt2 != nullptr && pt1 == pt2){
-            return true;
+        while(rabit != nullptr && rabit->next != nullptr){
+            tortoise = tortoise->next;
+            rabit = rabit->next->next;
+
+            if(rabit == tortoise){
+                return true;
+            } 
         }
         return false;
     }
